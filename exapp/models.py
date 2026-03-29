d2a1926a7223cf1310e7351a2eaa8cb3c1241d30
jamesMuriithi <jamesmuriithi777@gmail.com>
2026-03-29 15:37:23 +0300

    Add transaction type to Expense model and update serializer and view for totals

--- exapp/models.py
+++ exapp/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Expense(models.Model):
+    TYPES = [
+        ('INCOME', 'Income'),
+        ('EXPENSE', 'Expense'),
+    ]
     CATEGORY_CHOICES = [
         ('Food', 'Food'),
         ('Rent', 'Rent'),
@@ -14,6 +18,7 @@ class Expense(models.Model):
     title = models.CharField(max_length=200)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+    transaction_type = models.CharField(max_length=10, choices=TYPES)
     date = models.DateField(auto_now_add=True)
 
     def __str__(self):
--- exapp/views.py
+++ exapp/views.py
@@ -2,20 +2,36 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 from .serializers import *
 from django.db.models import Sum
+from django.utils import timezone
+from datetime import timedelta
+
+
 
 def expense_list(request):
+    today = timezone.now().date()
+    week_start = today - timedelta(days=today.weekday())
+    month_start = today.replace(day=1)
+    
+    transactions = Expense.objects.filter(user=request.user).order_by('-date')
+    
+    daily_total = transactions.filter(date=today).aggregate(Sum('amount'))['amount__sum'] or 0
+    weekly_total = transactions.filter(date=week_start).aggregate(Sum('amount'))['amount__sum'] or 0
+    monthly_total = transactions.filter(date=month_start).aggregate(Sum('amount'))['amount__sum'] or 0
+    
     expenses = Expense.objects.filter(user=request.user).order_by('-date')
-    total_balance = expenses.aggregate(Sum('amount'))['amount__sum'] or 0
+    daily_total = expenses.aggregate(Sum('amount'))['amount__sum'] or 0
     
     if request.method == "POST":
         serializer = ExpenseSerializer(data=request.POST)
         if serializer.is_valid():
             serializer.save(user=request.user)
             return redirect('home')
-      
+    
     return render(request, 'Home.html', {
         'expenses': expenses,
-        'total_balance': total_balance
+        'daily_total': daily_total,
+        'weekly_total': weekly_total,
+        'monthly_total': monthly_total,
     })
     
 def delete_expense(request, pk):
