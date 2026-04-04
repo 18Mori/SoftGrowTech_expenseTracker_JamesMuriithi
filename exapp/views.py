da85e436f732825bde15988c03d1b4b7f682c331
jamesMuriithi <jamesmuriithi777@gmail.com>
2026-04-04 19:51:31 +0300

    Refactor expense calculations in views; update Home template to display daily, weekly, and monthly net balances

--- exapp/views.py
+++ exapp/views.py
@@ -13,11 +13,16 @@ def expense_list(request):
     week_start = today - timedelta(days=today.weekday())
     month_start = today.replace(day=1)
     
-    expenses = Expense.objects.filter(user=request.user).order_by('-date')
+    expenses = Expense.objects.filter(user=request.user)
     
-    daily_total = expenses.filter(date=today).aggregate(Sum('amount'))['amount__sum'] or 0
-    weekly_total = expenses.filter(date__gte=week_start).aggregate(Sum('amount'))['amount__sum'] or 0
-    monthly_total = expenses.filter(date__gte=month_start).aggregate(Sum('amount'))['amount__sum'] or 0
+    def get_totals(queryset):
+        income = queryset.filter(transaction_type='INCOME').aggregate(Sum('amount'))['amount__sum'] or 0
+        expense = queryset.filter(transaction_type='EXPENSE').aggregate(Sum('amount'))['amount__sum'] or 0
+        return income, expense, (income - expense)
+    
+    daily_in, daily_out, daily_net = get_totals(expenses.filter(date=today))
+    weekly_in, weekly_out, weekly_net = get_totals(expenses.filter(date__gte=week_start))
+    monthly_in, monthly_out, monthly_net = get_totals(expenses.filter(date__gte=month_start))
     
     if request.method == "POST":
         serializer = ExpenseSerializer(data=request.POST)
@@ -30,9 +35,15 @@ def expense_list(request):
     
     return render(request, 'Home.html', {
         'expenses': expenses,
-        'daily_total': daily_total,
-        'weekly_total': weekly_total,
-        'monthly_total': monthly_total,
+        'daily_in': daily_in,
+        'daily_out': daily_out,
+        'daily_net': daily_net,
+        'weekly_in': weekly_in,
+        'weekly_out': weekly_out,
+        'weekly_net': weekly_net,
+        'monthly_in': monthly_in,
+        'monthly_out': monthly_out,
+        'monthly_net': monthly_net,
     })
     
 def delete_expense(request, pk):
