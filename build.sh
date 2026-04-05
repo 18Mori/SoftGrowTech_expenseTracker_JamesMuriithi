545108c97c715515b766dc556ce07cf477163027
jamesMuriithi <jamesmuriithi777@gmail.com>
2026-04-05 15:17:17 +0300

    Refactor build script for dependency installation; update ALLOWED_HOSTS for Vercel deployment; enhance WSGI application setup; specify Python runtime in Vercel configuration..(set deployment)

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
-pip install -r requirements.txt
-python manage.py makemigrations
-python manage.py migrate
+python -m pip install -r requirements.txt
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput --clear
